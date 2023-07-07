3566ee39841e8b8a0f73c1f35466823050a3b15a
KH <kkh96071@gmail.com>
2023-07-07 19:54:16 +0900

    [init]
    [init]

---
+++ ToyProject3/src/main/java/team7/example/ToyProject3/controller/ReplyController.java
@@ -0,0 +1,4 @@
+package team7.example.ToyProject3.controller;
+
+public class ReplyController {
+}
---
+++ ToyProject3/src/main/java/team7/example/ToyProject3/repository/MemberRepository.java
@@ -0,0 +1,4 @@
+package team7.example.ToyProject3.repository;
+
+public class MemberRepository {
+}

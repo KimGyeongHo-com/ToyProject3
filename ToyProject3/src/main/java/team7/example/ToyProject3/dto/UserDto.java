98df3b0727a8990fa1d868a00c01a247a6fb93da
KH <kkh96071@gmail.com>
2023-07-17 15:43:13 +0900

    [fix] UserDto
    UserDto vaildation 처리 및 불필요한 코드 제거

--- ToyProject3/src/main/java/team7/example/ToyProject3/dto/UserDto.java
+++ ToyProject3/src/main/java/team7/example/ToyProject3/dto/UserDto.java
@@ -3,42 +3,58 @@ package team7.example.ToyProject3.dto;
 import lombok.*;
 import team7.example.ToyProject3.domain.Role;
 import team7.example.ToyProject3.domain.User;
+import team7.example.ToyProject3.domain.UserRole;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.sql.Timestamp;
 
 
-@Getter
-@Setter
+@Data
 @ToString
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserDto {
     private Long id;
-    private String name;
-    private String password;
-    private Role role;
-    private String nickname;
+
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    private String name;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{8,20}",
+            message = "영문과 숫자가 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
+    private String password;
+
+    private Role role;
+
+    private UserRole userRole;
+
+
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    private String nickname;
+
+
 
     private Timestamp createdAt;
 
     private Timestamp updatedAt;
 
 
-    public User toEntity(){
+    public User toEntity(UserDto userDto){
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         User user = new User();
-        user.setName(name);
-        user.setNickname(nickname);
-        user.setPassword(password);
-        user.setEmail(email);
+        user.setName(userDto.getName());
+        user.setNickname(userDto.getNickname());
+        user.setPassword(userDto.getPassword());
+        user.setEmail(userDto.getEmail());
         user.setCreatedAt(timestamp);
         user.setUpdatedAt(timestamp);
-
-        if(email == "admin@example.com")
-            user.setRole(Role.ADMIN);
-        else
-            user.setRole(Role.USER);
-
+        user.setRole(userDto.getRole());
+        user.setUserRole(UserRole.NORMAL);
 
         return user;
     }

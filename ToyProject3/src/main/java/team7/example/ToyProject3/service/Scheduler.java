04d2b637322e618c480f5f2ba35e6a374b35d9f7
yeeun <dpdmstjs@gmail.com>
2023-07-19 20:23:59 +0900

    feat: 스케줄러 등록 기능 추가

---
+++ ToyProject3/src/main/java/team7/example/ToyProject3/service/Scheduler.java
@@ -0,0 +1,30 @@
+package team7.example.ToyProject3.service;
+
+import java.util.List;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import team7.example.ToyProject3.domain.user.User;
+import team7.example.ToyProject3.domain.user.UserRole;
+import team7.example.ToyProject3.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class Scheduler {
+
+	private final UserRepository userRepository;
+
+	@Scheduled(fixedDelay = 60000)
+	public void changeUserRole() {
+
+		List<User> newbiesWithTenOrMoreBoards = userRepository.findByUserRoleAndBoardCount(UserRole.NORMAL, 10);
+
+		for (User user : newbiesWithTenOrMoreBoards) {
+			user.setUserRole(UserRole.VIP);
+		}
+
+		userRepository.saveAll(newbiesWithTenOrMoreBoards);
+	}
+}

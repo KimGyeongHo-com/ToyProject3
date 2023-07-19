4112066ca8fd9e3047d6351a05756899c2fce31b
yeeun <dpdmstjs@gmail.com>
2023-07-19 20:30:55 +0900

    feat: 게시판 삭제 기능 추가

--- ToyProject3/src/main/java/team7/example/ToyProject3/repository/ReplyRepository.java
+++ ToyProject3/src/main/java/team7/example/ToyProject3/repository/ReplyRepository.java
@@ -16,6 +16,7 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
 	@Query("SELECT r FROM Reply r LEFT JOIN r.parentReply parentReply WHERE r.board.id = :boardId ORDER BY CASE WHEN parentReply IS NULL THEN r.id ELSE parentReply.id END DESC, r.createdAt ASC")
 	List<Reply> findAllByBoardIdOrderByParentAndCreatedAt(Long boardId);
 
+	List<Reply> findByBoardId(Long boardId);
 
 
 }
